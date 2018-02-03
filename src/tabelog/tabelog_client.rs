f872437722419e5a5106554532ac6fdcf6bbf559
atsushi130 <atsushi_main@outlook.jp>
2018-02-03 18:49:04 +0900

    implemented tabelog client

---
+++ src/tabelog/tabelog_client.rs
@@ -0,0 +1,24 @@
+
+// Copyright (c) 2018 Atsushi Miyake
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or http://apache.org/licenses/LICENSE-2.0>
+// or the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// This file may not be copied, modified, or distributed except according to those terms.
+
+use tabelog::search_condition::SearchCondition;
+use tabelog::tabelog_source::TabelogSource;
+use regex::Regex;
+
+pub struct TabelogClient;
+
+pub type SearchResult = Vec<String>;
+impl TabelogClient {
+    pub fn search(&self, search_condition: &SearchCondition) -> SearchResult {
+        let source = TabelogSource.get_source(search_condition);
+        let regex = Regex::new("data-detail-url=\"(?P<url>.*)\" data-rst-id").unwrap();
+
+        regex.captures_iter(&source).map(|capture| {
+            capture["url"].to_string()
+        }).collect()
+    }
+}
